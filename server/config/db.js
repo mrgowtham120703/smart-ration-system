6c9754c3a19fcb9d4545c4ad9c0b54aa51df921c
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-04 17:24:23 -0800

    config/db.js created

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const connectDB = async (uri) => {
+    await mongoose.connect(uri, {useNewUrlPaerser: true, useUnifiedTopology: true});
+    console.log('MongoDB Connected');
+};
+
+module.exports = connectDB;
