bde63d8cb32fd6892a1891e212bae388e0f872ba
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-04 18:20:26 -0800

    modes/User.js created

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    type: { type: String, enum: ['consumer', 'salesman'], required: true },
+
+    // Consumer fields
+
+    familyCardNumber: { type: String }, // Consumer only
+    povertyLevel: { type: String, enum: ['APL', 'BPL'] },
+    name: String,
+
+    // Salesman fields
+    shopNumber: { type: String },
+    passwordJash: String,
+
+});
+
+module.exports = mongoose.model('User', userSchema);
