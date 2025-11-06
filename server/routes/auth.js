a979269e50fb107aa13dfc564c9ad6f1e2b3bab8
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-06 15:34:50 -0800

    routes/auth.js Created

---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const authCtrl = require('../controllers/authController');
+
+router.post('./consumer-login', authCtrl.consumerLogin);
+router.post('./salesman-login', authCtrl.salesmanLogin);
+
+module.exports = router;
