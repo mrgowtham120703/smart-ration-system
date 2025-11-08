0b664ed0cb941b57263a2cc590dae4ab7d812db2
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-07 20:40:13 -0800

    pages/ConsumerHome.jsx Created & Mounted

--- client/smart-ration-system/src/App.jsx
+++ client/smart-ration-system/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import LoginConsumer from './pages/LoginConsumer';
 import LoginSalesman from './pages/LoginSalesman';
+import ConsumerHome from './pages/ConsumerHome';
 
 const App = () => (
   <div>
@@ -10,6 +11,7 @@ const App = () => (
     <Routes>
       <Route path='/' element={<LoginConsumer />} />
       <Route path='/salesman' element={<LoginSalesman />} />
+      <Route path='/home' element={<ConsumerHome />} />
     </Routes>
   </div>
 )
--- client/smart-ration-system/src/pages/ConsumerHome.jsx
+++ client/smart-ration-system/src/pages/ConsumerHome.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ConsumerHome = () => {
-  return (
-    <div>ConsumerHome</div>
-  )
-}
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='dlex flex-col items-center justify-center min-h-screen bg-gray-100'>
+            <div className='bg-white p-8 rounded-2xl shadow-md w-80 w-80 text-center'>
+                <h2 className='text-2xl font-bold mb-6'>Welcome Consumer</h2>
+                <button
+                    onClick={() => navigate("/view-products")}
+                    className='bg-blue-600 text-white w-full py-2 rounded-md mb-3 hover:bg-blue-700'
+                >View Products</button>
+                <button
+                    onClick={() => navigate("/select-products")}
+                    className='bg-green-600 text-white w-full py-2 rounded-md hover:bg-green-700'
+                >
+                    Selext Products
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default ConsumerHome
