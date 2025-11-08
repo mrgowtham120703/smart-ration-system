6be66bc943d497c2e783e98d1773690479c57c0e
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-07 20:42:22 -0800

    pages/ConsumerHome.jsx Errors Fixed

--- client/smart-ration-system/src/pages/ConsumerHome.jsx
+++ client/smart-ration-system/src/pages/ConsumerHome.jsx
@@ -6,7 +6,7 @@ const ConsumerHome = () => {
     const navigate = useNavigate();
 
     return (
-        <div className='dlex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
             <div className='bg-white p-8 rounded-2xl shadow-md w-80 w-80 text-center'>
                 <h2 className='text-2xl font-bold mb-6'>Welcome Consumer</h2>
                 <button
@@ -17,7 +17,7 @@ const ConsumerHome = () => {
                     onClick={() => navigate("/select-products")}
                     className='bg-green-600 text-white w-full py-2 rounded-md hover:bg-green-700'
                 >
-                    Selext Products
+                    Select Products
                 </button>
             </div>
         </div>
