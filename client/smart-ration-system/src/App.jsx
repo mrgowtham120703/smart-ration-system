68c90d27b17b7469f0889a9bcbc157356518747a
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-07 20:17:30 -0800

    pages/LoginConsumer.jsx Created & Mounted

--- client/smart-ration-system/src/App.jsx
+++ client/smart-ration-system/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import LoginConsumer from './pages/LoginConsumer';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<LoginConsumer />} />
+    </Routes>
+  </div>
+)
 
 export default App
--- client/smart-ration-system/src/pages/LoginConsumer.jsx
+++ client/smart-ration-system/src/pages/LoginConsumer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginConsumer = () => {
+
+   const [cardNumber, setCardNumber] = useState("");
+   const navigate = useNavigate();
+
+   const handleLogin = () => {
+    if(cardNumber.trim() !== ""){
+        alert("Consumer Login successfull");
+        navigate('/home');
+    } else {
+        alert("Please enter a vlaid ration card number")
+    }
+   };
+
   return (
-    <div>LoginConsumer</div>
-  )
-}
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div className='bg-white shadow-lg rounded-2xl p-8 w-80'>
+        <h2 className='text-xl font-semibold mb-4 text-center'>Consumer Login</h2>
+        <input
+        type='text'
+        value={cardNumber}
+        onChange={(e) => setCardNumber(e.target.value)}
+        placeholder='Enter a Ration Card Number'
+        className='border rounded-md px-3 py-2 w-full mb-4'
+        />
+        <button
+        onClick={handleLogin}
+        className='bg-blue-600 text-white w-full py-2 rounded-md hover:bg-blue-700'
+        >
+        Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default LoginConsumer
