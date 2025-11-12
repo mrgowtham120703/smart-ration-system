bc78a201319f5f4d0cef26f507028d4d4f92171a
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-11 21:15:36 -0800

    Added Icons In Navbar.jsx

--- client/smart-ration-system/src/components/Navbar.jsx
+++ client/smart-ration-system/src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
     <nav className='bg-emerald-800 flex justify-between items-center px-6 py-3'>
-      <h1 className='font-extrabold text-gray-200 text-lg'>SMART RATION</h1>
+      <h1 className='font-extrabold text-gray-200 text-lg'><i className="fa-solid fa-cart-shopping"></i> SMART RATION</h1>
       <div className='flex gap-4'>
-        <Link to="/" className='hover:underline decoration-2 text-gray-200 font-bold'>CONSUMER</Link>
-        <Link to="/salesman" className='hover:underline decoration-2 text-gray-200 font-bold'>SALESMAN</Link>
+        <Link to="/" className='hover:underline decoration-2 text-gray-200 font-bold'><i className="fa-regular fa-user"></i> CONSUMER</Link>
+        <Link to="/salesman" className='hover:underline decoration-2 text-gray-200 font-bold'><i className="fa-solid fa-user-tie"></i> SALESMAN</Link>
       </div>
     </nav>
 );
