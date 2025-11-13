5358afedf428e2f0f61c2e88347b914a2adc9d30
mrgowtham120703 <gowthammr120703@gmail.com>
2025-11-12 17:23:14 -0800

    Icons added in CartPage.jsx

--- client/smart-ration-system/src/pages/CartPage.jsx
+++ client/smart-ration-system/src/pages/CartPage.jsx
@@ -13,17 +13,17 @@ const CartPage = () => {
   const total = cart.reduce((acc, x) => acc + x.price * x.quantity, 0);
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-50 py-20">
-      <h2 className="text-2xl font-semibold mb-6">Your Cart</h2>
+    <div className="flex flex-col items-center min-h-screen bg-emerald-200 py-20">
+      <h2 className="text-2xl font-semibold mb-6"><i className="fa-solid fa-cart-shopping"></i> Your Cart</h2>
       <div className="w-80 space-y-3">
         {cart.map((item, i) => (
-          <div key={i} className="bg-white p-4 rounded-xl shadow flex justify=between">
+          <div key={i} className="bg-gray-100 p-4 rounded-xl shadow flex justify=between">
             <span>{item.name}</span>
-            <span>{item.quantity}kg x ₹{item.price} = ₹{item.quantity * item.price}</span>
+            <span>{item.quantity}kg <i className="fa-solid fa-xmark"></i> <i className="fa-solid fa-indian-rupee-sign"></i> {item.price} <i class="fa-solid fa-equals"></i> <i className="fa-solid fa-indian-rupee-sign"></i> {item.quantity * item.price}</span>
           </div>
         ))}
       </div>
-      <h3 className="mt-6 font-bold text-lg">Total: ₹{total}</h3>
+      <h3 className="mt-6 font-bold text-lg">Total: <i className="fa-solid fa-indian-rupee-sign"></i>{total}</h3>
       <div className="flex gap-3 mt-4">
         <button
           onClick={() => navigate('/select-products')}
